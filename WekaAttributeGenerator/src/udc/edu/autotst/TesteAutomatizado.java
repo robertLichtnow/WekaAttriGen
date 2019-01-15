10a75ae318fff133a7d0c38f036d91bf0382a401
robertLichtnow <matheusrobert8@gmail.com>
2019-01-14 22:10:20 -0200

    Criado script para comparação entre classificadores

--- WekaAttributeGenerator/src/udc/edu/autotst/TesteAutomatizado.java
+++ WekaAttributeGenerator/src/udc/edu/autotst/TesteAutomatizado.java
@@ -6,105 +6,79 @@ import java.util.Calendar;
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.trees.J48;
+import weka.classifiers.functions.Logistic;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
 public class TesteAutomatizado {
 	
 	public static void main(String[] args) throws Exception {
-		TesteAutomatizado.testeAutomatizadoLenses(2, 10);
+		TesteAutomatizado.testeAutomatizadoLenses(10, 20);
 	}
 	
-	public static void testeAutomatizadoLenses(int folds, int seed) throws Exception {
-		String pathArff = "C:\\Users\\mathlich\\Desktop\\datasets\\lenses.data.arff";
+	//Testes foram feitos utilizando um split de 70/30
+	//Os dados são armazenados em um arquivo csv
+	
+	public static void testeAutomatizadoLenses(int seed, double initialPercent) throws Exception {
+		String pathArff = "C:\\Users\\Robert\\Desktop\\datasets\\lenses.data.arff";
 		
 		ArffLoader loader = new ArffLoader();
 		loader.setFile(new File(pathArff));
 		
 		Instances ins = loader.getDataSet();
 		ins.setClassIndex(ins.numAttributes() - 1);
+		ins.randomize(new Random(seed));
+		
+		
+		
 		
 		String unique = TesteAutomatizado.generateUniqueString();
 		
-		PrintWriter pw = new PrintWriter(new File("C:\\Users\\mathlich\\Desktop\\resultados\\Resultados-" + unique + ".csv"));
+		Logistic classifier = new Logistic();
+		
+		PrintWriter pw = new PrintWriter(new File("C:\\Users\\Robert\\Desktop\\resultados\\Resultados-" + classifier.getClass().getSimpleName() + "-" + unique + ".csv"));
 		
 		pw.print("Classifier;");
-		pw.print("Binary Splits;");
-		pw.print("Pruning Confidence;");
-		pw.print("Laplace Smoothing;");
-		pw.print("Folds;");
+		pw.print("Split;");
 		pw.print("Correct;");
-		pw.print("Incorrect;");
 		pw.print("Pct Correct;");
-		pw.print("Pct Incorrect;");
-		pw.print("Kappa;");
-		pw.print("Mean absolute error;");
-		pw.print("Root mean squared error;");
-		pw.print("Relative absolute error;");
-		pw.print("Root relative squared error;");
-		pw.print("Total number of Instances;");
-		pw.print("Area under ROC;");
-		pw.print("Area under PRC;");
-		pw.print("Area under weighted ROC;");
-		pw.print("Area under weighted PRC");
+		pw.print("Stddev;");
+		pw.print("Variance;");
+		pw.print("Total number of Instances");
 		pw.print("\n");
 		
-		J48 classifier = new J48();
 		
-		Double prune = new Double(0.1);
+		
 		
 		Evaluation eval = null;
 			
-		while(prune <= 0.5) {
+		while(initialPercent <= 90) {
 			
-			String options[] = {"-B","-C",prune.toString()};
+			int trainSize = (int) Math.round(ins.numInstances() * initialPercent / 100);
+			int testSize = ins.numInstances() - trainSize;
 			
-			classifier.setOptions(options);
+			Instances train = new Instances(ins, 0, trainSize);
+			Instances test = new Instances(ins,trainSize, testSize); 
 			
-			eval = new Evaluation(ins);
+			eval = new Evaluation(train);
 			
-			eval.crossValidateModel(classifier, ins, folds, new Random(10));
+			classifier.buildClassifier(train);
+			
+			eval.evaluateModel(classifier, test);
 			
 			pw.print(classifier.getClass().getSimpleName() + ";");
-			pw.print(TesteAutomatizado.hasOption(classifier.getOptions(), "-B") ? "Sim;" : "Não;");
-			pw.print(prune.toString().replace(".", ",") + ";");
-			pw.print(TesteAutomatizado.hasOption(classifier.getOptions(), "-A") ? "Sim;" : "Não;");
-			pw.print(folds + ";");
+			pw.print(Double.toString(initialPercent).replace(".", ",") + ";");
 			pw.print((eval.correct() + ";").replace(".", ","));
-			pw.print((eval.incorrect() + ";").replace(".", ","));
 			pw.print((eval.pctCorrect() + ";").replace(".", ","));
-			pw.print((eval.pctIncorrect() + ";").replace(".", ","));
-			pw.print((eval.kappa() + ";").replace(".", ","));
-			pw.print((eval.meanAbsoluteError() + ";").replace(".", ","));
 			pw.print((eval.rootMeanSquaredError() + ";").replace(".", ","));
-			pw.print((eval.relativeAbsoluteError() + ";").replace(".", ","));
-			pw.print((eval.rootRelativeSquaredError() + ";").replace(".", ","));
+			pw.print(((eval.rootMeanSquaredError() * eval.rootMeanSquaredError()) + ";").replace(".", ","));
 			pw.print((ins.numInstances() + ";").replace(".", ","));
 			
-			String ROC = "";
-			String PRC = "";
-			String WROC = "";
-			String WPRC = "";
 			
-			try {ROC = new Double(eval.areaUnderROC(ins.numAttributes()-1)).toString();}
-			catch(Exception e) {}
-			try {PRC = new Double(eval.areaUnderPRC(ins.numAttributes()-1)).toString();}
-			catch(Exception e) {}
-			try {WROC = new Double(eval.weightedAreaUnderROC()).toString();}
-			catch(Exception e) {}
-			try {WPRC = new Double(eval.weightedAreaUnderPRC()).toString();}
-			catch(Exception e) {}
-			
-			
-			pw.print(ROC.replace(".", ",") + ";");
-			pw.print(PRC.replace(".", ",") + ";");
-			pw.print(WROC.replace(".", ",") + ";");
-			pw.print(WPRC.replace(".", ",") + "");
 			
 			pw.print("\n");
 			
-			prune += 0.1;
+			initialPercent += 10;
 			
 		}
 		
@@ -125,8 +99,7 @@ public class TesteAutomatizado {
 		unique += cal.get(Calendar.DATE) + "T";
 		unique += cal.get(Calendar.HOUR_OF_DAY) + "-";
 		unique += cal.get(Calendar.MINUTE) + "-";
-		unique += cal.get(Calendar.SECOND) + ".";
-		unique += cal.get(Calendar.MILLISECOND);
+		unique += cal.get(Calendar.SECOND);
 
 		return unique;
 	}
