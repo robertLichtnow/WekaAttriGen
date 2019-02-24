611e4dae2130fa10f64a3f63599e7e7979f74b9e
robertLichtnow <matheusrobert8@gmail.com>
2019-02-24 15:17:25 -0300

    Hotfix de Serializable e adicionado projeto de geração de modelos

---
+++ WekaAttriGenTreinamento/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+import udc.edu.weka.classifiers.J48;
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		ArffLoader loader = new ArffLoader();
+		loader.setSource(new File("datasets/lenses.data.arff"));
+		
+		Instances ins = loader.getDataSet();
+		ins.setClassIndex(ins.numAttributes()-1);
+		
+		Classifier cls = new J48();
+		cls.buildClassifier(ins);
+		
+		weka.core.SerializationHelper.write("models/lenses-j48.model",cls);
+	}
+}
